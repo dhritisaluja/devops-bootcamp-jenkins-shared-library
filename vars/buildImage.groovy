982c8ff85cc7f2dfcb842bba8d63120b1114f963
Dhriti Saluja <dhritisaluja1999@gmail.com>
2025-04-24 16:42:49 +0530

    Updated buildImage.groovy file

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,7 +2,7 @@
 def call(String imageName) {
     echo 'building the docker image...'
     withCredentials([
-            usernamePassword(credentials: '<credentials-id>', usernameVariable: 'USERNAME', passwordVariable: 'PWD')
+            usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USERNAME', passwordVariable: 'PWD')
     ]) {
         sh "docker build -t $imageName ."
         sh "echo $PWD | docker login -u $USERNAME --password-stdin"
